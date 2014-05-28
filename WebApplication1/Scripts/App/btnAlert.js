b92a4dfff0ae8277b2dccd40d7adf02547024072
sharker86 <joan.isaac.collado@gmail.com>
2014-05-28 14:09:04 +0200

    v6.1

--- WebApplication1/Scripts/App/btnAlert.js
+++ WebApplication1/Scripts/App/btnAlert.js
@@ -16,13 +16,13 @@ function valida_envia() {
     //Creamos un objeto 
     object = document.fvalida.email;
     valueForm = object.value;
-
+    var b = false;
     // Patron para el correo
     var patron = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     if (valueForm.search(patron) != 0) {
         alert("Mail incorrecto")
         object.style.color = "#f00";
-        return 0;
+        b = true;
     }
 
     //valido el nombre 
@@ -30,16 +30,17 @@ function valida_envia() {
         alert("Tiene que escribir su nombre")
         object.style.color = "#f00";
         document.fvalida.nombre.focus()
-        return 0;
+        b = true;
     }
 
     if (document.fvalida.razon.value.length == 0) {
         alert("Tiene que escribir una razon")
         object.style.color = "#f00";
         document.fvalida.razon.focus()
-        return 0;
+        b = true;
     }
-
+    if (b == true)
+        return 0;
     //el formulario se envia 
     alert("Muchas gracias por enviar el formulario");
     document.fvalida.submit();
