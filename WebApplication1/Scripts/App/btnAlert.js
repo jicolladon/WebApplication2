06284eb57222d75d93ca6ad7cace3cd50ae8aa5c
sharker86 <joan.isaac.collado@gmail.com>
2014-05-28 14:39:01 +0200

    v6

--- WebApplication1/Scripts/App/btnAlert.js
+++ WebApplication1/Scripts/App/btnAlert.js
@@ -11,3 +11,36 @@ $(function () {
         }
     });
 });
+
+function valida_envia() {
+    //Creamos un objeto 
+    object = document.fvalida.email;
+    valueForm = object.value;
+
+    // Patron para el correo
+    var patron = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    if (valueForm.search(patron) != 0) {
+        alert("Mail incorrecto")
+        object.style.color = "#f00";
+        return 0;
+    }
+
+    //valido el nombre 
+    if (document.fvalida.nombre.value.length == 0) {
+        alert("Tiene que escribir su nombre")
+        object.style.color = "#f00";
+        document.fvalida.nombre.focus()
+        return 0;
+    }
+
+    if (document.fvalida.razon.value.length == 0) {
+        alert("Tiene que escribir una razon")
+        object.style.color = "#f00";
+        document.fvalida.razon.focus()
+        return 0;
+    }
+
+    //el formulario se envia 
+    alert("Muchas gracias por enviar el formulario");
+    document.fvalida.submit();
+}
