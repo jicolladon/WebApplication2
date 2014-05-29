7437e1a71be8c9ad3a017141d09b6f9253f71b30
sharker86 <joan.isaac.collado@gmail.com>
2014-05-29 15:51:27 +0200

    v10

---
+++ WebApplication1/Migrations/201405261235437_InitialCreate.cs
@@ -0,0 +1,31 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clientes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(nullable: false),
+                        Apellido = c.String(),
+                        Edad = c.Int(nullable: false),
+                        Activo = c.Boolean(nullable: false),
+                        Email = c.String(),
+                        Sueldo = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Clientes");
+        }
+    }
+}
